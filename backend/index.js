8c27a54996a92fee65a6ef3eb0ec49c870991816
Varun Pradeep <varunpradeep30@gmail.com>
2022-10-10 16:06:47 +0530

    project files added to repo

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const db = require('./models');
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() =>{
+    app.listen(3001, ()=>{
+        console.log("Running");
+    });
+});
+
+
+
