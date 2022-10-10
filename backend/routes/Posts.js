1a910bf1d89127ab4e37010c70172a84cbe7ac4d
Varun Pradeep <varunpradeep30@gmail.com>
2022-10-10 16:43:13 +0530

    nodemon issue fixed

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res) => {
-    res.send('Hello world');
+router.get("/",(req, res) => {
+    res.send('hello');
 });
-
-router.post();
 
 module.exports = router;
