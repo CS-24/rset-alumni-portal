8b8695ef7da44766b9ae00cacf4c92f8fb23f13f
Varun Pradeep <varunpradeep30@gmail.com>
2022-11-10 23:19:49 +0530

    add get request for posts by user

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,24 +1,34 @@
-const { application } = require('express');
-const express = require('express');
+const { application } = require("express");
+const express = require("express");
 const router = express.Router();
 
 const { Posts } = require("../models");
 
 router.get("/", async (req, res) => {
-    const postList = await Posts.findAll();
-    res.json(postList);
+	const postList = await Posts.findAll();
+	res.json(postList);
+});
+
+router.get("/byUser/:userId", async (req, res) => {
+	const userId = req.params.userId;
+	const postList = await Posts.findAll({
+		where: {
+			UserId: userId
+		},
+	});
+	res.json(postList);
 });
 
 router.get("/byId/:id", async (req, res) => {
-    const id = req.params.id;
-    const post = await Posts.findByPk(id);
-    res.json(post);
+	const id = req.params.id;
+	const post = await Posts.findByPk(id);
+	res.json(post);
 });
 
 router.post("/", async (req, res) => {
-    const post = req.body;
-    await Posts.create(post);
-    res.json(post);
+	const post = req.body;
+	await Posts.create(post);
+	res.json(post);
 });
 
 module.exports = router;
